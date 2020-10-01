358ab1fe0a7faee77e6cdd3b53ed80437ccab3b5
Arthur Lui <luiarthur@gmail.com>
2020-09-30 22:54:26 -0700

    working on coroutine tfp hurdle version

--- dev/tfp/tfp_model_coroutine.py
+++ dev/tfp/tfp_model_coroutine.py
@@ -6,6 +6,10 @@ from tensorflow_probability import bijectors as tfb
 dtype = np.float64
 Root = tfd.JointDistributionCoroutine.Root
 
+import sys
+sys.path.append("../pystan")
+import simulate_data
+
 def compute_gamma_T_star(gamma_C, gamma_T, p):
     return p * gamma_T + (1 - p) * gamma_C
 
@@ -13,44 +17,86 @@ def compute_eta_T_star(gamma_C, gamma_T, eta_C, eta_T, p, gamma_T_star):
     numer = eta_T * (1 - gamma_T) * p + eta_C * (1 - p) * (1 - gamma_C)
     return numer / (1 - gamma_T_star)
 
-def mix(gamma, eta, loc, scale, neg_inf, name):
-    return tfd.Mixture(
-        cat=tfd.Categorical(probs=np.array([gamma, 1 - gamma])),
-        components=[
-            tfd.Deterministic(np.float64(neg_inf)),
-            tfd.MixtureSameFamily(
-                mixture_distribution=tfd.Categorical(probs=eta),
-                components_distribution=tfd.Normal(loc=loc, scale=scale)
-            )
-        ],
-    name=name)
+def mix(n, eta, loc, scale, name):
+    return tfd.Sample(
+        tfd.MixtureSameFamily(
+            mixture_distribution=tfd.Categorical(probs=eta),
+            components_distribution=tfd.Normal(loc=loc, scale=scale),
+            name=name),
+        sample_shape=n)
 
 # NOTE:
 # - `Sample` and `Independent` resemble, respectively, `filldist` and `arraydist` in Turing.
 # - JointDistributionCoroutine is good in this case,
 #   otherwise, I usually use JointDistributionNamed.
-def create_model(K, neg_inf=-100, dtype=np.float64):
+def create_model(nC, nT, n0C, n0T, K, dtype=np.float64):
+    nposC = nC - n0C
+    nposT = nT - n0T
+
     def _model():
         p = yield Root(tfd.Beta(dtype(1), dtype(1), name="p"))
-        gamma_C = yield Root(tfd.Gamma(dtype(1), dtype(1), name="gamma_C"))
-        gamma_T = yield Root(tfd.Gamma(dtype(1), dtype(1), name="gamma_T"))
-        eta_C = yield Root(tfd.Dirichlet(np.ones(K, dtype=dtype) / K, name="eta_C"))
-        eta_T = yield Root(tfd.Dirichlet(np.ones(K, dtype=dtype) / K, name="eta_T"))
-        loc = yield Root(tfd.Sample(tfd.Normal(dtype(0), dtype(1)), sample_shape=K, name="loc"))
-        sigma_sq = yield Root(tfd.Sample(tfd.InverseGamma(dtype(3), dtype(2)), sample_shape=K,
+        gamma_C = yield Root(tfd.Beta(dtype(1), dtype(1), name="gamma_C"))
+        gamma_T = yield Root(tfd.Beta(dtype(1), dtype(1), name="gamma_T"))
+        eta_C = yield Root(tfd.Dirichlet(np.ones(K, dtype=dtype) / K,
+                                         name="eta_C"))
+        eta_T = yield Root(tfd.Dirichlet(np.ones(K, dtype=dtype) / K,
+                                         name="eta_T"))
+        loc = yield Root(tfd.Sample(tfd.Normal(dtype(0), dtype(1)),
+                                    sample_shape=K, name="loc"))
+        sigma_sq = yield Root(tfd.Sample(tfd.InverseGamma(dtype(3), dtype(2)),
+                                         sample_shape=K,
                               name="sigma_sq"))
         scale = np.sqrt(sigma_sq)
-        y_C = yield mix(gamma_C, eta_C, loc, scale, neg_inf, name="y_C")
+
+        gamma_T_star = compute_gamma_T_star(gamma_C, gamma_T, p)
+        eta_T_star = compute_eta_T_star(gamma_C[..., tf.newaxis],
+                                        gamma_T[..., tf.newaxis],
+                                        eta_C, eta_T,
+                                        p[..., tf.newaxis],
+                                        gamma_T_star[..., tf.newaxis])
+
+        # likelihood
+        y_C = yield mix(nC, eta_C, loc, scale, name="y_C")
+        n0C = yield tfd.Binomial(nC, gamma_C, name="n0C")
+        y_T = yield mix(nT, eta_T_star, loc, scale, name="y_T")
+        n0T = yield tfd.Binomial(nT, gamma_T_star, name="n0T")
+
         # TODO:
         # - y_T
         # - implement skew-t distribution
         # - prior for skew, nu parameter
+
     return tfd.JointDistributionCoroutine(_model)
 
-model = create_model(5)
-model.sample()
-model.sample(2)
+# Test compile. 
+model = create_model(9, 11, 3, 6, 5)
+s = model.sample(); s
+s = model.sample(10); s
+model.log_prob(s)
 
+# Bijectors.
+bijector = [
+    tfb.Sigmoid(),  # p
+    tfb.Sigmoid(),  # gamma_C
+    tfb.Sigmoid(),  # gamma_T
+    tfb.SoftmaxCentered(),  # eta_C
+    tfb.SoftmaxCentered(),  # eta_T
+    tfb.Identity(),  # loc
+    tfb.Exp()  # sigma_sq
+]
 
-m = mix(0.5, [.1, .3, .6], np.zeros(3), np.ones(3), -10, name="x")
-m.sample(5)
+data = simulate_data.gen_data(
+    n_C=1000, n_T=1000, p=0.95, gamma_C=.3, gamma_T=.2, K=2,
+    scale=np.array([0.7, 1.3]), nu=np.array([15, 30]),
+    loc=np.array([1, -1]), phi=np.array([-2, -5]),
+    eta_C=np.array([.99, .01]), eta_T=np.array([.01, .99]), seed=1)
+
+# TODO:
+# - Try to do HMC/NUTS/ADVI.
+# - Need to implement skew-t.
+
+# nC = data['y_C']
+# nT = data['y_T']
+# ypos_C = data['y_C']
+# model = create_model(nC, nT, n0C, n0T, K, dtype=np.float64):
+
