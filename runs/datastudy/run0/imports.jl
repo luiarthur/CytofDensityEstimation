76c4275a962cd17179dcc9376343fc3ed4c5a06b
Arthur Lui <luiarthur@gmail.com>
2020-10-19 17:33:37 -0700

    fixed typos

---
+++ runs/datastudy/run0/imports.jl
@@ -0,0 +1,104 @@
+ENV["GKSwstype"] = "nul"  # For StatsPlots
+
+import Pkg; Pkg.activate(joinpath(@__DIR__, "../../../"))
+using CSV, DataFrames
+using CytofDensityEstimation
+const CDE = CytofDensityEstimation
+using Distributions
+import Random
+using BSON
+using StatsPlots
+
+using RCall
+@rimport stats as rstats 
+
+
+function postprocess(chain, laststate, summarystats, yC, yT;
+                     ygrid=collect(range(-8, 8, length=1000)), bw_postpred=0.2)
+  # Print KS Statistic.
+  ks_fit = rstats.ks_test(yC, yT)
+  println("KS-test p-value: ", ks_fit["p.value"][1])
+  flush(stdout)
+
+  # Print summary statistics.
+  CDE.Model.printsummary(chain, summarystats)
+  flush(stdout)
+
+  # Plot results.
+  @time CDE.Model.plotpostsummary(chain, summarystats, yC, yT, imgdir,
+                                  bw_postpred=bw_postpred, ygrid=ygrid)
+  flush(stdout)
+
+  # Send results
+  if awsbucket != nothing
+    CDE.Util.s3sync(from=resultsdir, to=awsbucket, tags=`--exclude '*.nfs'`)
+  end
+end
+
+
+function defaults(yC, yT, K; seed=nothing)
+  seed == nothing || Random.seed!(seed)
+
+  data = CDE.Model.Data(yC, yT)
+  prior = CDE.Model.Prior(K, mu=CDE.Model.compute_prior_mu(data),
+                          nu=LogNormal(1.6, 0.4), p=Beta(100, 100),
+                          omega=InverseGamma(.1, .1), psi=Normal(-1, 1))
+  state = CDE.Model.State(data, prior)
+  tuners = CDE.Model.Tuners(K, 0.1)
+  return (state=state, data=data, prior=prior, tuners=tuners)
+end
+
+
+function run(config)
+  resultsdir = config[:resultsdir]
+  mkpath(resultsdir)
+  path_to_log = joinpath(resultsdir, "log.txt")
+  CDE.Util.redirect_stdout_to_file(path_to_log) do
+    _run(config)
+  end
+end
+
+function _run(config)
+  println("pid: $(getpid())"); flush(stdout)
+
+  resultsdir = config[:resultsdir]
+  awsbucket = config[:awsbucket]
+  yC = config[:yC]
+  yT = config[:yT]
+  K = config[:K]
+  nsamps = config[:nsamps]
+  nburn = config[:nburn]
+
+  # Make image path id needed.
+  imgdir = joinpath(resultsdir, "img")
+  mkpath(imgdir)
+
+  # Create initial state, data, prior, and tuners objects.
+  state, data, prior, tuners = defaults(yC, yT, K, seed=0)
+  println("Priors:")
+  foreach(fn -> println(fn, " => ", getfield(prior, fn)),
+          fieldnames(CDE.Model.Prior))
+  
+  # Run analysis.
+  println("Run Chain ..."); flush(stdout)
+  @time chain, laststate, summarystats = CDE.Model.fit(
+      state, data, prior, tuners, nsamps=[nsamps], nburn=nburn, thin=1,
+      rep_aux=10, rep_beta_flipped=50)
+  flush(stdout)
+
+  # Save results
+  BSON.bson("$(resultsdir)/results.bson",
+            Dict(:chain=>chain, :laststate=>laststate, :data=>data,
+                 :prior=>prior, :summarystats=>summarystats))
+
+  # Load data:
+  out = BSON.load("$(resultsdir)/results.bson")
+
+  # Post process
+  postprocess(out[:chain], out[:laststate], out[:summarystats], out[:data].yC,
+              out[:data].yT; ygrid=collect(range(-8, 8, length=1000)),
+              bw_postpred=0.2)
+
+  # Print done.
+  println("Done!"); flush(stdout)
+end
---
+++ runs/datastudy/run0/run.jl
@@ -0,0 +1,40 @@
+include(joinpath(@__DIR__, "imports.jl"))  # For precompile.
+markers = [:CD3z, :EOMES, :Perforin, :Granzyme_A, :Siglec7]
+
+using Distributed
+rmprocs(workers())
+addprocs(length(markers))
+@everywhere include(joinpath(@__DIR__, "imports.jl"))
+
+# Read command line args.
+if length(ARGS) > 1
+  resultsdir = ARGS[1]
+  awsbucket = ARGS[2]
+  println("ARGS: ", ARGS); flush(stdout)
+  nsamps = 10000
+  nburn = 2000
+else
+  resultsdir = "results/test/"
+  awsbucket = nothing
+  nsamps = 10
+  nburn = 20
+end
+
+# Read data.
+donor = 1
+path_to_data = "../../../data/TGFBR2/cytof-data/donor$(donor).csv"
+subsample_size = 20000
+data = CDE.Util.subsample(DataFrame(CSV.File(path_to_data)), subsample_size,
+                          seed=0)
+configs = [let
+             _awsbucket = (awsbucket == nothing) ? awsbucket : "$(awsbucket)/donor$(donor)/$(marker)"
+             yC, yT = CDE.Util.partition(data, marker)
+             (yC=yC, yT=yT, K=5, nsamps=nsamps, nburn=nburn,
+              awsbucket=_awsbucket,
+              resultsdir=joinpath(resultsdir, "donor$(donor)/$(marker)"))
+           end for marker in markers]
+
+# Parallel run.
+println("Starting runs ...")
+res = pmap(run, configs, on_error=identity)
+println("DONE WITH ALL!"); flush(stdout)
