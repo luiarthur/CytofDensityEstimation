ef001ffa6ba5f4aab5f35ae5c2e1d74e1b106608
Arthur Lui <luiarthur@gmail.com>
2020-10-31 16:37:27 -0700

    another bug fix

--- runs/simstudy/sim5/sim.jl
+++ runs/simstudy/sim5/sim.jl
@@ -77,8 +77,8 @@ res = pmap(c -> let
   resd1 = c[2][:resultsdir]
   out0 = BSON.load("$(resd0)/results.bson")
   out1 = BSON.load("$(resd1)/results.bson")
-  imgdir = "$(c[1][:imgdir])../../img"; mkpath(imgdir)
-  bucket = "$(c[1][:awsbucket])../../img"
+  imgdir = "$(c[1][:imgdir])/../../img"; mkpath(imgdir)
+  bucket = "$(c[1][:awsbucket])/../../img"
   # bucket = awsbucket == nothing ? awsbucket : "$(awsbucket)/K$(K)/scenario$(snum)/img"
   postprocess(out0[:chain], out1[:chain], out0[:data], 
               imgdir, bucket, simdata=c[1][:simdata],
