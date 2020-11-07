536b66e05e5ddb8faca6964b91574aa58e3c6d28
Arthur Lui <luiarthur@gmail.com>
2020-11-07 15:46:13 -0800

    prep for datarun

--- runs/datastudy/run1/imports.jl
+++ runs/datastudy/run1/imports.jl
@@ -1,47 +1,122 @@
 ENV["GKSwstype"] = "nul"  # For StatsPlots
 
 import Pkg; Pkg.activate(joinpath(@__DIR__, "../../../"))
-using CSV, DataFrames
 using CytofDensityEstimation
 const CDE = CytofDensityEstimation
+using LaTeXStrings
 using Distributions
-import Random
-using BSON
 using StatsPlots
-
+import Random
 using RCall
+using BSON
+using StatsFuns
 @rimport stats as rstats 
+VD = Vector{Dict{Symbol, Any}}
+
+ygrid = collect(range(-10, 8, length=200))
+default_plotsize = (400, 400)
+flatten = Iterators.flatten
+
+
+# Plot observed data histogram.
+function plot_observed_hist(yC, yT, imgdir; bins, binsT=nothing,
+                            alpha=0.3, digits=5, plotsize=default_plotsize,
+                            legendpos=:topleft)
+  binsC = bins
+  binsT == nothing && (binsT = bins)
+  histogram(yC[isfinite.(yC)], bins=binsC, label=L"y_C", alpha=alpha,
+            color=:blue, linealpha=0, normalize=true)
+  histogram!(yT[isfinite.(yT)], bins=binsT, label=L"y_T", alpha=alpha,
+             color=:red, linealpha=0, normalize=true)
+  p0C = round(mean(isinf.(yC)), digits=digits)
+  p0T = round(mean(isinf.(yT)), digits=digits)
+  title!("prop. -∞ in C: $(p0C) | prop. -∞ in T: $(p0T)", titlefont=font(10))
+  plot!(size=plotsize, legend=:topleft)
+  savefig(joinpath(imgdir, "data-hist.pdf"))
+  closeall()
+end
 
 
 function postprocess(chain, laststate, summarystats, yC, yT, imgdir;
                      bw_postpred=0.2, density_legend_pos=:best,
-                     ygrid=collect(range(-8, 8, length=1000)))
+                     simdata=nothing, ygrid=collect(range(-8, 8, length=1000)))
+  # Print summary statistics.
+  CDE.Model.printsummary(chain, summarystats)
+  flush(stdout)
+
   # Print KS Statistic.
   ks_fit = rstats.ks_test(yC, yT)
   println("KS-test p-value: ", ks_fit["p.value"][1])
   flush(stdout)
 
-  # Print summary statistics.
-  CDE.Model.printsummary(chain, summarystats)
-  flush(stdout)
-
   # Plot results.
   @time CDE.Model.plotpostsummary(chain, summarystats, yC, yT, imgdir,
                                   bw_postpred=bw_postpred, ygrid=ygrid,
-                                  density_legend_pos=density_legend_pos)
+                                  density_legend_pos=density_legend_pos,
+                                  simdata=simdata)
   flush(stdout)
 end
 
+
+function postprocess(chain0, chain1, data, imgdir, awsbucket;
+                     simdata=nothing, bw_postpred=0.20,
+                     density_legend_pos=:best, binsC=nothing, binsT=nothing,
+                     ygrid=collect(range(-8, 8, length=1000)), p=nothing)
+  mkpath(imgdir)
+  CDE.Util.redirect_stdout_to_file(joinpath(imgdir, "bf.txt")) do
+    println("Start merging results...")
+
+    # Compute Bayes factor in favor of M1.
+    _p = CDE.Model.group(:p, chain0)[1]
+    p == nothing || (_p = p)
+    @time lbf = CDE.Model.log_bayes_factor(data, VD(chain0[1]), VD(chain1[1]))
+    pm1 = CDE.Model.posterior_prob1(lbf, logpriorodds=logit(_p))
+    println("Log Bayes Factor: $(lbf) | P(M1|y): $(pm1)")
+
+    # Print KS Statistic.
+    ks_fit = rstats.ks_test(data.yC, data.yT)
+    println("KS-test p-value: ", ks_fit["p.value"][1])
+    flush(stdout)
+
+    # Plot marginal posterior density
+    CDE.Model.plot_posterior_predictive(data.yC, data.yT, chain0, chain1, pm1,
+                                        imgdir, bw_postpred=bw_postpred,
+                                        density_legend_pos=density_legend_pos,
+                                        simdata=simdata, lw=.5, ls=:solid, 
+                                        binsC=binsC, binsT=binsT,
+                                        xlims_=(-6, 6), digits=5, fontsize=7)
+
+    CDE.Model.plot_gamma(data.yC, data.yT, chain0, chain1, pm1, imgdir)
+
+    # DIC
+    dic0, dic1 = CDE.dic(chain0[1], data), CDE.dic(chain1[1], data)
+    println("(DIC0, DIC1): ($(round(dic0, digits=3)), $(round(dic1, digits=3)))")
+
+    dic_average = CDE.dic(chain0[1], chain1[1], pm1, data)
+    println("DIC average: $(round(dic_average, digits=3))")
+
+    flush(stdout)
+  end
+
+  # Send results
+  if awsbucket != nothing
+    CDE.Util.s3sync(from=imgdir, to=awsbucket, tags=`--exclude '*.nfs'`)
+  end
+end
 
 function defaults(yC, yT, K; seed=nothing)
   seed == nothing || Random.seed!(seed)
 
   data = CDE.Model.Data(yC, yT)
-  prior = CDE.Model.Prior(K, mu=CDE.Model.compute_prior_mu(data),
-                          nu=LogNormal(1.6, 0.4), p=Beta(100, 100),
-                          omega=InverseGamma(.1, .1), psi=Normal(-1, 1))
+  prior = CDE.Model.Prior(K, mu=Normal(0, 3),
+                          nu=LogNormal(3, .5),
+                          p=Beta(100, 100),
+                          psi=Normal(-1, .5), 
+                          a_omega=2.5,
+                          tau=Gamma(.5, 1.),
+                          eta=Dirichlet(K, 1/K))
   state = CDE.Model.State(data, prior)
-  tuners = CDE.Model.Tuners(K, 0.1)
+  tuners = CDE.Model.Tuners(K, 1.0)
   return (state=state, data=data, prior=prior, tuners=tuners)
 end
 
@@ -65,6 +140,7 @@ function _run(config)
   K = config[:K]
   nsamps = config[:nsamps]
   nburn = config[:nburn]
+  thin = config[:thin]
 
   # Make image path id needed.
   imgdir = joinpath(resultsdir, "img")
@@ -72,16 +148,21 @@ function _run(config)
 
   # Create initial state, data, prior, and tuners objects.
   state, data, prior, tuners = defaults(yC, yT, K, seed=0)
+  state.p = config[:p]
+  state.beta = config[:beta]
   println("Priors:")
   foreach(fn -> println(fn, " => ", getfield(prior, fn)),
           fieldnames(CDE.Model.Prior))
-  
+
+  # Plot data.
+  plot_observed_hist(yC, yT, imgdir, bins=50, binsT=50*2,
+                     alpha=0.3, digits=5, legendpos=:topleft)
+ 
   # Run analysis.
   println("Run Chain ..."); flush(stdout)
-  @time chain, laststate, summarystats = CDE.ppfit(state, data, prior, tuners,
-                                                   p=0.5, nsamps=[nsamps],
-                                                   nburn=nburn, thin=1,
-                                                   rep_aux=10, warmup=1000)
+  @time chain, laststate, summarystats = CDE.fit(
+      state, data, prior, tuners, nsamps=[nsamps], fix=[:p, :beta],
+      nburn=nburn, thin=thin, rep_aux=1, flags=CDE.Model.Flag[])
   flush(stdout)
 
   # Save results
@@ -94,8 +175,8 @@ function _run(config)
 
   # Post process
   postprocess(out[:chain], out[:laststate], out[:summarystats], out[:data].yC,
-              out[:data].yT, imgdir; bw_postpred=0.2,
-              ygrid=collect(range(-8, 8, length=1000)),
+              out[:data].yT, imgdir, bw_postpred=0.3, 
+              ygrid=collect(range(-8, 8, length=200)),
               density_legend_pos=:topleft)
 
   # Send results
--- runs/datastudy/run1/run.jl
+++ runs/datastudy/run1/run.jl
@@ -3,30 +3,23 @@ markers = [:CD3z, :EOMES, :Perforin, :Granzyme_A, :Siglec7]
 
 using Distributed
 rmprocs(workers())
-addprocs(length(markers))
+addprocs(length(markers) * 2)
 @everywhere include(joinpath(@__DIR__, "imports.jl"))
-
-postprocess_only = false
 
 # Read command line args.
 if length(ARGS) > 1
   resultsdir = ARGS[1]
   awsbucket = ARGS[2]
   println("ARGS: ", ARGS); flush(stdout)
-  nsamps = 10000
-  nburn = 2000
+  nsamps = 5000
+  nburn = 6000
+  istest=false
 else
   resultsdir = "results/test/"
   awsbucket = nothing
   nsamps = 10
   nburn = 20
-
-  if postprocess_only
-    SCRATCH_DIR = ENV["SCRATCH_DIR"]
-    SIMNAME = "run1"
-    resultsdir = "$(SCRATCH_DIR)/cde/datastudy/$(SIMNAME)"
-    awsbucket = "s3://cytof-density-estimation/datastudy/$(SIMNAME)"
-  end
+  istest=true
 end
 
 # Read data.
@@ -35,32 +28,41 @@ path_to_data = "../../../data/TGFBR2/cytof-data/donor$(donor).csv"
 subsample_size = 20000
 data = CDE.Util.subsample(DataFrame(CSV.File(path_to_data)), subsample_size,
                           seed=0)
-configs = [let
-             _awsbucket = (awsbucket == nothing) ? awsbucket : "$(awsbucket)/donor$(donor)/$(marker)"
-             yC, yT = CDE.Util.partition(data, marker, log_response=true)
-             (yC=yC, yT=yT, K=5, nsamps=nsamps, nburn=nburn, marker=marker,
-              awsbucket=_awsbucket,
-              resultsdir=joinpath(resultsdir, "donor$(donor)/$(marker)"))
-           end for marker in markers]
+configs = [[let
+  bucket = (awsbucket == nothing) ? awsbucket : "$(awsbucket)/donor$(donor)/$(marker)"
+  yC, yT = CDE.Util.partition(data, marker, log_response=true)
+  (yC=yC, yT=yT, K=6, nsamps=nsamps, nburn=nburn, marker=marker,
+   awsbucket=bucket, p=0.5, beta=beta, thin=1, 
+   resultsdir=joinpath(resultsdir, "donor$(donor)/$(marker)"))
+end for beta in 0:1] for marker in markers]
 
 # Parallel run.
-println("Starting runs ...")
-if !postprocess_only  # Do run as well.
-  res = pmap(run, configs, on_error=identity)
-else
-  # Post processing only.
-  res = pmap(resdir -> let
-               out = BSON.load("$(resdir)/results.bson")
-               imgdir = joinpath(resdir, "img"); mkpath(imgdir)
-               postprocess(out[:chain], out[:laststate], out[:summarystats],
-                           out[:data].yC, out[:data].yT, imgdir; bw_postpred=0.2,
-                           ygrid=collect(range(-8, 8, length=1000)),
-                           density_legend_pos=:topleft)
-             end, getfield.(configs, :resultsdir), on_error=identity)
-end
+println("Starting runs ..."); flush(stdout)
+res = pmap(run, istest ? configs[1] : flatten(configs), on_error=identity)
 
-println("Status of runs:")
+println("Status of runs:"); flush(stdout)
 foreach(z -> println(z[2], " => ", z[1]),
-        zip(res, getfield.(configs, :resultsdir)))
+        zip(res, getfield.(istest ? configs[1] : flatten(configs), :resultsdir)))
+
+# Bayes factor
+println("Compute BF:"); flush(stdout)
+res = pmap(c -> let
+  # NOTE
+  @assert length(unique(getfield.(c, :marker))) == 1
+  resd0 = c[1][:resultsdir]
+  resd1 = c[2][:resultsdir]
+  out0 = BSON.load("$(resd0)/results.bson")
+  out1 = BSON.load("$(resd1)/results.bson")
+  imgdir = "$(c[1][:imgdir])/../../img"; mkpath(imgdir)
+  bucket = c[1][:awsbucket] == nothing ? nothing : "$(c[1][:awsbucket])/../img"
+  postprocess(out0[:chain], out1[:chain], out0[:data], 
+              imgdir, bucket, simdata=c[1][:simdata],
+              density_legend_pos=:topleft, bw_postpred=.3, binsC=50, binsT=100,
+              p=c[1][:p])
+end, istest ? [configs[1]] : configs, on_error=identity)
+
+println("Status of BF computation:"); flush(stdout)
+foreach(z -> println(z[2], " => ", z[1]),
+        zip(res, [c[1][:marker] for c in (istest ? [configs[1]] : configs)]))
 
 println("DONE WITH ALL!"); flush(stdout)
