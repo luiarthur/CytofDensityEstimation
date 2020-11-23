4564730ee5eb9807269fbc1491c1861394490dab
Arthur Lui <luiarthur@gmail.com>
2020-11-23 13:59:22 -0800

    added mean check

--- dev/AmhTest/amh_test.jl
+++ dev/AmhTest/amh_test.jl
@@ -9,9 +9,17 @@ import Pkg; Pkg.activate(".")
 include("amh.jl")
 using StatsPlots
 using MCMCChains
+import Random
 
-K = 32
-meantrue = zeros(K)
+Random.seed!(0)
+
+trilindex(x) = x[tril(ones(Int, size(x))) .== 1]
+Base.range(x::AbstractArray) = [minimum(x), maximum(x)]
+# x = reshape(collect(1:9), 3, 3)
+# trilindex(x)
+
+K = 6
+meantrue = randn(K) # zeros(K)
 covtrue = let
   M = randn(K,K)
   M*M'
@@ -20,16 +28,20 @@ end
 m = DensityModel(x -> logpdf(MvNormal(meantrue, covtrue), x))
 # p = RWMH(MvNormal(K, .1))
 p = RWAM(K)
-c = sample(m, p, Int(1e5), chain_type=Chains);
-c = c[50000:10:end];
+c = sample(m, p, Int(60000), chain_type=Chains);
+c = c[30001:10:end];
 
 plot(vec(get(c, :lp)[1].data), label=nothing)
-mean(c).nt[:mean]
+meanx = mean(c).nt[:mean]
 
 X = c.value.data[:, 1:end-1, 1]
 covtrue
 cov(X)
-scatter(covtrue, cov(X), label=nothing, color=:blue, alpha=.3)
+scatter(trilindex(covtrue), trilindex(cov(X)), label=nothing, color=:blue, alpha=.6)
+plot!(range(covtrue), range(covtrue), ls=:dot, label=nothing)
 corrplot(X[:, 1:3], grid=false)
+
+scatter(meantrue, meanx, label=nothing, color=:blue, alpha=.6)
+plot!(range(meantrue), range(meantrue), ls=:dot, label=nothing)
 
 # boxplot(X, label=nothing)
